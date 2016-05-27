f487404331acff70b1681003116dccaff2138eaa
Ilin, Aleksandr2 <a2.ilin@itransition.com>
2016-05-27 17:29:50 +0300

    ~migrations (psr)

--- app/DoctrineMigrations/Version20160527165503.php
+++ app/DoctrineMigrations/Version20160527165503.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * PHP version: 5.6+
+ */
 namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
@@ -17,27 +19,31 @@ class Version20160527165503 extends AbstractMigration
      * Before up
      *
      * @param Schema $schema
+     * 
      * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
      */
     public function preUp(Schema $schema)
     {
         // platform checking
         if ($this->connection->getDatabasePlatform()->getName() != 'mysql') {
-            $this->abortIf(true, "Migration can only be executed safely on 'mysql'.");
+            $this->abortIf(true, "Migration [UP} can only be executed safely on 'mysql'");
         }
     }
 
     /**
      * Migration up
+     *
      * @param Schema $schema
      */
     public function up(Schema $schema)
     {
-        $this->addSql("
-          ALTER TABLE tblProductData
-            ADD intStock INT NOT NULL,
-            ADD floatCostInGBP NUMERIC(15, 2) NOT NULL
-        ");
+        $this->addSql(
+            "
+          ALTER TABLE `tblProductData`
+            ADD COLUMN `intStock` INT NOT NULL AFTER `stmTimestamp`,
+            ADD COLUMN `floatCostInGBP` DECIMAL(15,2) NOT NULL AFTER `intStock`;
+        "
+        );
 
     }
 
@@ -45,13 +51,14 @@ class Version20160527165503 extends AbstractMigration
      * Before down
      *
      * @param Schema $schema
+     *
      * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
      */
     public function preDown(Schema $schema)
     {
         // platform checking
         if ($this->connection->getDatabasePlatform()->getName() != 'mysql') {
-            $this->abortIf(true, "Migration can only be executed safely on 'mysql'.");
+            $this->abortIf(true, "Migration [DOWN] can only be executed safely on 'mysql'");
         }
     }
 
@@ -62,10 +69,12 @@ class Version20160527165503 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE tblProductData
-              DROP intStock,
-              DROP floatCostInGBP
-        ");
+        $this->addSql(
+            "
+            ALTER TABLE `tblProductData`
+              DROP COLUMN `intStock`,
+              DROP COLUMN `floatCostInGBP`;
+        "
+        );
     }
 }
