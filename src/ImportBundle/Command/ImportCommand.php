493e03a0c2691d22c58b28d5be91b19fc1b29eab
Ilin, Aleksandr2 <a2.ilin@itransition.com>
2016-05-30 13:43:14 +0300

    current version

--- src/ImportBundle/Command/ImportCommand.php
+++ src/ImportBundle/Command/ImportCommand.php
@@ -3,33 +3,22 @@
 namespace ImportBundle\Command;
 
 use Ddeboer\DataImport\Filter\CallbackFilter;
-use Ddeboer\DataImport\Filter\ValidatorFilter;
 use Ddeboer\DataImport\ItemConverter\CallbackItemConverter;
 use Ddeboer\DataImport\ItemConverter\MappingItemConverter;
 use Ddeboer\DataImport\Reader\CsvReader;
-use Ddeboer\DataImport\ValueConverter\ArrayValueConverterMap;
 use Ddeboer\DataImport\ValueConverter\CallbackValueConverter;
-use Ddeboer\DataImport\ValueConverter\DateTimeValueConverter;
-use Ddeboer\DataImport\ValueConverter\MappingValueConverter;
-use Ddeboer\DataImport\ValueConverter\StringToObjectConverter;
 use Ddeboer\DataImport\Workflow;
-use Ddeboer\DataImport\Writer\ConsoleTableWriter;
+use Ddeboer\DataImport\Writer\ArrayWriter;
 use Ddeboer\DataImport\Writer\DoctrineWriter;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Validator\Validator;
 
 class ImportCommand extends ContainerAwareCommand
 {
-    protected $debug = false;
-
-    protected $testMode = false;
-
     protected function configure()
     {
        $this
@@ -40,25 +29,19 @@ class ImportCommand extends ContainerAwareCommand
                 "Path to import file", "vagrant/task/stock.csv")
             ->addOption(
                 'test-mode', 't', InputOption::VALUE_NONE,
-                "If set, application will not make changes in db")
-            ->addOption('debug', 'd', InputOption::VALUE_NONE,
-                "Debug mode will show you what's really happen");
+                "If set, application will not make changes in db");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if ($input->getOption('debug')) {
-            $this->debug = true;
-        }
-
         if (!file_exists($input->getOption('file'))) {
             throw new RuntimeException(sprintf("File %s doesn't exists", $input->getOption('file')));
         }
 
-        $output->writeln("Hello, I'm a first console application");
-
         $file = new \SplFileObject($input->getOption('file'));
         $reader = new CsvReader($file);
+
+        $output->writeln(sprintf("Importing %s...", $file->getBasename()));
 
         $reader->setHeaderRowNumber(0);
 
@@ -66,16 +49,15 @@ class ImportCommand extends ContainerAwareCommand
 
         $entityManager = $this->getContainer()->get('doctrine')->getEntityManager();
 
-        $doctrineWriter = new DoctrineWriter($entityManager, 'ImportBundle:Tblproductdata');
-        $workflow->addWriter($doctrineWriter);
+        if ($input->getOption('test-mode')) {
+            $testData = array();
 
-        /*$output = new ConsoleOutput();
+            $writer = new ArrayWriter($testData);
+        } else {
+            $writer = new DoctrineWriter($entityManager, 'ImportBundle:Tblproductdata');
+        }
 
-        $consoleWriter = new ConsoleTableWriter($output, new Table($output));
-
-
-
-        $workflow->addWriter($consoleWriter);*/
+        $workflow->addWriter($writer);
 
         $converter = new MappingItemConverter();
         $converter
@@ -84,7 +66,7 @@ class ImportCommand extends ContainerAwareCommand
             ->addMapping('Product Description', 'strproductdesc')
             ->addMapping('Stock', 'intstock')
             ->addMapping('Cost in GBP', 'floatcostingbp')
-            ->addMapping('Discontinued', 'dtmDiscontinued');
+            ->addMapping('Discontinued', 'dtmdiscontinued');
 
         $workflow->addItemConverter($converter);
         $workflow->setSkipItemOnFailure(true);
@@ -93,50 +75,53 @@ class ImportCommand extends ContainerAwareCommand
             $item['dtmadded'] = new \DateTime();
             $item['stmtimestamp'] = new \DateTime();
 
-            /*if (!empty($item['dtmDiscontinued'])) {
-                $item['dtmDiscontinued'] = new \DateTime();
-            } else {
-                $item['dtmDiscontinued'] = null;
-            }*/
-
             return $item;
         });
 
-       $workflow->addFilter(
-            new CallbackFilter(function ($data) {
-                return is_numeric($data['Cost in GBP']);
-            })
-        );
+        $skipped = array();
 
         $workflow->addFilter(
-            new CallbackFilter(function ($data) {
-                return $data['Cost in GBP'] > 5  && $data['Stock'] > 10;
+            new CallbackFilter(function ($data) use (&$skipped, $reader) {
+                if (is_numeric($data['Cost in GBP'])
+                    && is_numeric($data['Stock'])
+                    && ($data['Cost in GBP'] > 5 && $data['Cost in GBP'] < 1000)
+                    && $data['Stock'] > 10
+                ) {
+                    return true;
+                }
+
+                array_push($skipped, $reader->current());
+                return false;
             })
         );
-
-        $workflow->addFilter(
-            new CallbackFilter(function ($data) {
-                return $data['Cost in GBP'] < 1000;
-            })
-        );
-
-        $dt = new DateTimeValueConverter('\y\e\s', 'Y-m-d');
-
-        $workflow->addValueConverter("dtmDiscontinued", $dt);
-            /*new CallbackValueConverter(function ($item) {
-                return date("Y-m-d", time());
-            }));*/
 
         $workflow->addItemConverter($converter);
 
-        
+        $workflow->addValueConverter(
+            "dtmdiscontinued", new CallbackValueConverter(function ($item) {
+                if (empty($item)) {
+                    return null;
+                }
 
+                return new \DateTime();
+            })
+        );
 
+        $result = $workflow->process();
 
+        if ($skipped) {
+            foreach ($skipped as $data) {
+                $output->writeln(sprintf("[Skipped]: %s", implode(":", $data)));
+            }
+        }
 
-
-
-
-        $workflow->process();
+        $output->writeln(
+            sprintf(
+                "Done: Total - %s, Success: %s, Skipped: %s",
+                $reader->count(),
+                $result->getSuccessCount(),
+                $reader->count() - $result->getSuccessCount()
+            )
+        );
     }
 }
